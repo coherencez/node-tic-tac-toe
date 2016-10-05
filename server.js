82debdf5f8bf748edbc69aab0b96e6eb1e879e5e
Thomas Swann <tkswann@mac.com>
2016-10-04 21:23:25 -0500

    fixed error in server.js, connected stylesheet and client-side js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const server = Server(app)
 const io = socketio(server)
 
 const PORT = process.env.PORT || 3000
-const MONGODB_URL = process.end.MONGODB_URL || 'mongodb://localhost:27017/tictactoe'
+const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/tictactoe'
 
 app.set('view engine', 'pug')
 
