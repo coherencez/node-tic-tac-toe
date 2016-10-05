776ce0baaa83509880c8faefbb9c24092d03f853
Thomas Swann <tkswann@mac.com>
2016-10-04 21:28:41 -0500

    basic socket.io implementation

--- public/main.js
+++ public/main.js
@@ -1,2 +1,7 @@
 'use strict'
 console.log('hello from client-side js')
+
+const socket = io()
+
+socket.on('connect', () => console.log(`Socket connected: ${socket.id}`))
+socket.on('disconnect', () => console.log(`Socket disconnected: ${socket.id}`))
